ea679e4875bb32e4a30137deb02be8c04808b631
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-15 20:33:49 +0800

    fix: updated users layout

--- web/app/(users)/layout.tsx
+++ web/app/(users)/layout.tsx
@@ -1,5 +1,13 @@
+import { SiteFooter } from '@/components/layouts/footer/site-footer';
+import { SiteHeader } from '@/components/layouts/header/site-header';
 import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div className='relative flex min-h-screen flex-col'>
+      <SiteHeader />
+      <div className='flex-1'>{children}</div>
+      <SiteFooter />
+    </div>
+  );
 }
