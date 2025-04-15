e62cb64469c42e235aca6ed1de6c502826aa378d
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-15 21:49:46 +0800

    fix: centered footer

--- web/components/layouts/footer/site-footer.tsx
+++ web/components/layouts/footer/site-footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function SiteFooter() {
   return (
     <footer className='border-t py-6 md:py-0'>
-      <div className='container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row'>
+      <div className='container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row mx-auto'>
         <div className='flex items-center gap-2'>
           <Coins className='h-5 w-5' />
           <p className='text-sm leading-loose text-center md:text-left'>
