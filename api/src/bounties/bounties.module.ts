ca07a87de580308d774fd5cc63831ac195c70625
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-16 10:36:02 +0800

    fix: resolved related issues

--- api/src/bounties/bounties.module.ts
+++ api/src/bounties/bounties.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { BountiesService } from './bounties.service';
+import { PrismaService } from 'src/prisma.service';
 import { BountiesController } from './bounties.controller';
+import { BountiesService } from './bounties.service';
 
 @Module({
   controllers: [BountiesController],
-  providers: [BountiesService],
+  providers: [BountiesService, PrismaService],
 })
 export class BountiesModule {}
