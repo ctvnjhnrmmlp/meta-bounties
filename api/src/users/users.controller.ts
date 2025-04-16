fd48cc707d5290417c37e5f1bcb34daab3690afd
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-16 10:05:46 +0800

    fix: updated user controller

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,34 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Prisma } from 'generated/prisma';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: Prisma.UserCreateInput) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    return this.usersService.users({
+      skip: 0,
+      take: 1,
+      cursor: {
+        id: '',
+      },
+      where: {},
+    });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.user({
+      id,
+    });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') id: string, @Body() data: Prisma.UserUpdateInput) {
+    return this.usersService.updateUser({
+      where: {
+        id,
+      },
+      data,
+    });
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.deleteUser({
+      id,
+    });
   }
 }
